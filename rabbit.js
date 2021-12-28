d1231da0b5a71684f709931c6ba43dfa98b0a6e9
Themoun-zoe <zoewoozoe@gmail.com>
2021-12-28 13:21:37 +0900

    first commit

---
+++ rabbit.js
@@ -0,0 +1,9 @@
+$('#info-Btn').click(function(){
+    $('#myModal').css({display: "block"});
+    console.log("hello");
+  });
+
+
+  $('#closeBtn').click(function(){
+    $('#myModal').css({display: "none"});
+  });
